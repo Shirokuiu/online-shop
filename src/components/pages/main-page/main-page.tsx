4b5efa9c786e8b8472c3881cb5e10062a8ba2a7c
pavelosipov <osipov_p_v@ntik.ru>
2022-12-24 21:17:32 +0300

    Добавил шрифты и глобальные стили

--- src/components/pages/main-page/main-page.tsx
+++ src/components/pages/main-page/main-page.tsx
@@ -1,23 +1,14 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
-import CenterLayout from 'src/components/layouts/center-layout/center-layout';
-import PageLayout from 'src/components/layouts/page-layout/page-layout';
+import './main-page.scss';
 
 function MainPage() {
   return (
     <HelmetProvider>
-      <PageLayout
-        renderFooter={() => (
-          <CenterLayout>
-            <h2>Контент футера</h2>
-          </CenterLayout>
-        )}
-      >
-        <Helmet>
-          <title>Руководство</title>
-        </Helmet>
-        <CenterLayout />
-      </PageLayout>
+      <Helmet>
+        <title>Руководство</title>
+      </Helmet>
+      <span className="test">test</span>
     </HelmetProvider>
   );
 }
